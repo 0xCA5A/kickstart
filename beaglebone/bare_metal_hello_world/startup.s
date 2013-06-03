e7af1a21eca9905865f87ee788defcd76bc1a92f
Samuel Casa <samuelcasa9@gmail.com>
2013-06-03 16:15:51 +0200

    beaglebone: bare metal c example source modified, exception vector table removed
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- beaglebone/bare_metal_hello_world/startup.s
+++ beaglebone/bare_metal_hello_world/startup.s
@@ -1,16 +1,7 @@
 .section INTERRUPT_VECTOR, "x"
-.global _reset
-_reset:
-  B reset_handler /* reset */
-  B . /* undefined instruction */
-  B . /* swi */
-  B . /* prefetch abort */
-  B . /* data abort */
-  B . /* reserved */
-  B . /* irq */
-  B . /* fiq */
+.global _asm_entry
 
-reset_handler:
+_asm_entry:
   LDR sp, =stack_top /* set stack pointer */
   BL c_entry /* c code entry point */
   B . /* loop forever after return from c code */

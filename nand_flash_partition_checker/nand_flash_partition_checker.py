b0501d3b24fc5e30eb65a6a260ade1c83480dcb1
Samuel Casa <samuelcasa9@gmail.com>
2014-02-19 09:44:18 +0100

    nand flash partition checker: summary added, minor changes in code formatting
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- nand_flash_partition_checker/nand_flash_partition_checker.py
+++ nand_flash_partition_checker/nand_flash_partition_checker.py
@@ -6,83 +6,95 @@ import sys
 
 
 class NANDFlashPartition(object):
-    def __init__(self, nand_flash_configuration, partition_name, size_in_KiB):
+    """class to store nand flash partition information"""
+    def __init__(self, nand_flash_configuration, partition_name, size_in_kib):
         #print "[i] hello from NANDFlashPartition constructor"
 
         # check block alignment
-        if size_in_KiB % nand_flash_configuration.get_block_size_in_KiB() != 0 :
-            print "[!] error in alignment, %d not a multiple of %d" % (size_in_KiB, nand_flash_configuration.get_block_size_in_KiB())
+        if size_in_kib % nand_flash_configuration.get_block_size_in_kib() != 0 :
+            print "[!] error in alignment, %d not a multiple of %d" % (size_in_kib, nand_flash_configuration.get_block_size_in_kib())
             sys.exit(1)
 
         self._nand_flash_configuration = nand_flash_configuration
         self._partition_name = partition_name
-        self._size_in_KiB = size_in_KiB
+        self._size_in_kib = size_in_kib
 
     def get_partition_name(self):
+        """partition name accessor"""
         return self._partition_name
 
-    def get_size_in_KiB(self):
-        return self._size_in_KiB
+    def get_size_in_kib(self):
+        """size accessor"""
+        return self._size_in_kib
 
     def get_size_in_blocks(self):
-        return self._size_in_KiB / self._nand_flash_configuration.get_block_size_in_KiB()
+        """size accessor"""
+        return self._size_in_kib / self._nand_flash_configuration.get_block_size_in_kib()
 
 
 class NANDFlashConfiguration(object):
-    def __init__(self, page_size_in_KiB, block_size_in_KiB, nand_flash_size_in_KiB, nand_base_address):
+    """class to store nand flash configuration as erase block - and page size"""
+    def __init__(self, page_size_in_kib, block_size_in_kib, nand_flash_size_in_kib, nand_base_address):
         print "[i] hello from NANDFlashConfiguration constructor"
-        print "    page size: %dKiB" % (page_size_in_KiB)
-        print "    block size: %dKiB" % (block_size_in_KiB)
-        print "    nand flash size: %dKiB" % (nand_flash_size_in_KiB)
+        print "    page size: %dkib" % (page_size_in_kib)
+        print "    block size: %dkib" % (block_size_in_kib)
+        print "    nand flash size: %dkib" % (nand_flash_size_in_kib)
         print "    nand base address: 0x%08X" % (nand_base_address)
-        self._page_size_in_KiB = page_size_in_KiB
-        self._block_size_in_KiB = block_size_in_KiB
-        self._nand_flash_size_in_KiB = nand_flash_size_in_KiB
+        self._page_size_in_kib = page_size_in_kib
+        self._block_size_in_kib = block_size_in_kib
+        self._nand_flash_size_in_kib = nand_flash_size_in_kib
         self._nand_base_address = nand_base_address
 
-    def get_page_size_in_KiB(self):
-        return self._page_size_in_KiB
+    def get_page_size_in_kib(self):
+        """page size accessor"""
+        return self._page_size_in_kib
 
-    def get_block_size_in_KiB(self):
-        return self._block_size_in_KiB
+    def get_block_size_in_kib(self):
+        """block size accessor"""
+        return self._block_size_in_kib
 
-    def get_nand_flash_size_in_KiB(self):
-        return self._nand_flash_size_in_KiB
+    def get_nand_flash_size_in_kib(self):
+        """size accessor"""
+        return self._nand_flash_size_in_kib
 
     def get_nand_base_address(self):
+        """base address accessor"""
         return self._nand_base_address
 
 
 class NANDFlashPartitionChecker(object):
-    """class to design / check a nand flash partition layout..."""
+    """class to design / check a nand flash partition layout, block aligned..."""
 
     def __init__(self):
+        """constructor, init nand flash partition list"""
         print '[i] hello from NANDFlashPartitionChecker constructor, i am so happy to be here...'
         self.nand_flash_partition_list = []
 
-    def addNANDPartition(self, nand_flash_partition):
-        print '[i] add NAND partition: size %dKiB, identifier %s' % (nand_flash_partition.get_size_in_KiB(), nand_flash_partition.get_partition_name())
+    def add_nand_partition(self, nand_flash_partition):
+        """add nand partition to local list"""
+        print '[i] add NAND partition: size %dkib, identifier %s' % (nand_flash_partition.get_size_in_kib(), nand_flash_partition.get_partition_name())
         self.nand_flash_partition_list.append(nand_flash_partition)
 
-    def printNANDPartitionLayout(self):
+    def print_nand_partition_layout(self, nand_flash_configuration):
+        """print partition layout using the partition information added to the local list (using add_nand_partition function)"""
         print '[i] NAND partition layout:'
 
 
         partition_index = 0
         offset_in_blocks = 0
-        offset_in_KiB = 0
+        offset_in_kib = 0
 
         print "-------------------------------------------------------------------------------------------------------------"
-        print "index\t\t   identifier\tstart addr\tend addr\toffset\t\tsize [KiB]\tsize [blocks]"
+        print "index\t\t  identifier\tstart addr\tend addr\toffset\t\tsize [kib]\tsize [blocks]"
         print "-------------------------------------------------------------------------------------------------------------"
         for nand_flash_partition in self.nand_flash_partition_list:
 
             if nand_flash_partition.get_size_in_blocks() != 0:
-                size_in_KiB = nand_flash_partition.get_size_in_KiB()
+                size_in_kib = nand_flash_partition.get_size_in_kib()
                 size_in_blocks = nand_flash_partition.get_size_in_blocks()
             else:
-                size_in_KiB = nand_flash_configuration.get_nand_flash_size_in_KiB() - offset_in_KiB
-                size_in_blocks = nand_flash_configuration.get_nand_flash_size_in_KiB() / nand_flash_configuration.get_block_size_in_KiB() - offset_in_blocks
+                size_in_kib = nand_flash_configuration.get_nand_flash_size_in_kib() - offset_in_kib
+                size_in_blocks = nand_flash_configuration.get_nand_flash_size_in_kib() / nand_flash_configuration.get_block_size_in_kib() - offset_in_blocks
 
                 # check for configuration errors
                 if partition_index != (len(self.nand_flash_partition_list) - 1):
@@ -91,37 +103,47 @@ class NANDFlashPartitionChecker(object):
 
             print " %2d%25s\t0x%08X\t0x%08X\t0x%08X\t%d\t\t%d" % (partition_index,
                 nand_flash_partition.get_partition_name(),
-                offset_in_KiB * 1024 + nand_flash_configuration.get_nand_base_address(),
-                offset_in_KiB * 1024 + nand_flash_partition.get_size_in_KiB() * 1024 + nand_flash_configuration.get_nand_base_address() - 1,
-                offset_in_KiB * 1024,
-                size_in_KiB,
+                offset_in_kib * 1024 + nand_flash_configuration.get_nand_base_address(),
+                offset_in_kib * 1024 + nand_flash_partition.get_size_in_kib() * 1024 + nand_flash_configuration.get_nand_base_address() - 1,
+                offset_in_kib * 1024,
+                size_in_kib,
                 size_in_blocks)
 
             partition_index += 1
-            offset_in_KiB += size_in_KiB
+            offset_in_kib += size_in_kib
             offset_in_blocks += size_in_blocks
 
+        print "\nsummary:"
+        print "nand flash size:\t\t\t%dkib, %d blocks (block size: %dkib)" % (nand_flash_configuration.get_nand_flash_size_in_kib(),
+            nand_flash_configuration.get_nand_flash_size_in_kib() / nand_flash_configuration.get_block_size_in_kib(),
+            nand_flash_configuration.get_block_size_in_kib())
+        print "partitioned nand flash size:\t\t%dkib, %d blocks (block size: %dkib)" % (offset_in_kib,
+            offset_in_blocks,
+            nand_flash_configuration.get_block_size_in_kib())
+        print "free / unpartitioned nand flash size:\t%dkib, %d blocks (block size: %dkib)" % (nand_flash_configuration.get_nand_flash_size_in_kib() - offset_in_kib,
+            nand_flash_configuration.get_nand_flash_size_in_kib() / nand_flash_configuration.get_block_size_in_kib() - offset_in_blocks,
+            nand_flash_configuration.get_block_size_in_kib())
 
 
 if __name__ == "__main__":
 
-    nand_flash_configuration = NANDFlashConfiguration(2, 128, 128 * 1024, 0x81000000)
-    checker = NANDFlashPartitionChecker()
+    MY_NAND_FLASH_CONFIGURATION = NANDFlashConfiguration(2, 128, 128 * 1024, 0x00000000)
+    CHECKER = NANDFlashPartitionChecker()
 
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "SPL", 128))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "SPL Backup 1", 128))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "SPL Backup 2", 128))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "SPL Backup 3", 128))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "U-Boot", 2 * 1024))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "U-Boot Environment", 128))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "Rescue Linux Kernel", 3 * 1024))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "Rescue Root File System", 16 * 1024))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "Linux Kernel", 3 * 1024))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "Root File System", 64 * 1024))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "User Data", 4 * 1024))
-    checker.addNANDPartition(NANDFlashPartition(nand_flash_configuration, "System Data", 0))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "SPL", 128))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "SPL Backup 1", 128))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "SPL Backup 2", 128))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "SPL Backup 3", 128))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "U-Boot", 512))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "U-Boot Environment", 128))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "Rescue Linux Kernel", 3 * 1024))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "Rescue Root File System", 16 * 1024))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "Linux Kernel", 3 * 1024))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "Root File System", 64 * 1024))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "User Data", 4 * 1024))
+    CHECKER.add_nand_partition(NANDFlashPartition(MY_NAND_FLASH_CONFIGURATION, "System Data", 0))
 
-    checker.printNANDPartitionLayout()
+    CHECKER.print_nand_partition_layout(MY_NAND_FLASH_CONFIGURATION)
 
 
 

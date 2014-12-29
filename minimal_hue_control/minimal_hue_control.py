e138cac08bedaff248aa68529d633ec63f9742a3
Samuel Casa <samuelcasa9@gmail.com>
2014-12-29 17:14:08 +0100

    [mhc] static color definition
    Signed-off-by: Samuel Casa <samuelcasa9@gmail.com>

--- minimal_hue_control/minimal_hue_control.py
+++ minimal_hue_control/minimal_hue_control.py
@@ -1,57 +1,26 @@
 #!/usr/bin/env python
 
-"""Simple pyhue wrapper to easily change the color a bulb identified by its name
+"""simple application to easily change the bulb color of a connected bulb
+
+TODO(casasam): write which lib we wrap and where this guy comes from
 
 Usage:
     minimal_hue_control.py --bulb=NAME (--set-color=COLOR | --turn-off) [--address=ADDRESS] [--log=LEVEL]
-    minimal_hue_control.py --connect [--log=LEVEL]
+    minimal_hue_control.py --connect [--address=ADDRESS] [--log=LEVEL]
     minimal_hue_control.py --list-bulbs [--address=ADDRESS] [--log=LEVEL]
     minimal_hue_control.py --list-colors [--log=LEVEL]
 
-
 Options:
-    --address=ADDRESS           HUE bridge address
-    --bulb=NAME                 bulb identifier by name
+    --bulb=NAME                 bulb name
+    --set-color=COLOR           set color and turn light on
     --turn-off                  turn bulb off
-    --set-color=COLOR           set color and urn light on
+    --address=ADDRESS           HUE bridge address
+    --log=LEVEL                 set module log level
+    --connect                   register application at HUE bridge (necessary only once)
+    --list-bulbs                list known bulbs
     --list-colors               list supported colors
-    --connect                   register application at HUE bridge
-    --list-bulbs                list connected bulbs
-    --debug                     show debug output
-
-
-    light pattern proposal for a software build with tests:
-
-    idle:                                   do nothing, show last set bulb color
-    build job running:                      yellow
-    software build failed:                  red
-    bbs run failed:                         violet
-
-
-    GET LIST OF PYTHON COLOR DEFNITION < CHECK COLOR DEFINITION IN PYTHON
-
-    software build and bbs run successful: green
-
-    software build
-    build successful: green
-
-
-    led set color
-
-    get list of lights connected to the bridge
-    enable light
-    disable light
-    set color to light
-
-
-    http://anotherpythonprogrammer.blogspot.de/2013/05/pyhue-python-library-for-philips-hue.html
-
-
-    may: fix address for this hue guy??
-
-
-
 """
+
 
 import docopt
 import logging
@@ -61,67 +30,55 @@ import sys
 import pprint
 import collections
 
+
 # configure module logger, default log level is configured to info
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-
 def _main(_cli_arguments):
 
-    print "[DEBUG] remove this"
-    pprint.pprint(_cli_arguments)
-
-    # try to set the log level from the command line argument, not set if not valid
-    # the defined log levels in the logging module are: 'CRITICAL', 'DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN' etc.
+    # set the log level from the command line argument, not set if not valid
+    # valid log levels (from python logging module):
+    #   'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'
     _log_level = _cli_arguments['--log']
     if _log_level and hasattr(logging, _log_level):
         logger.setLevel(_log_level)
 
     _list_supported_colors = _cli_arguments['--list-colors']
     if _list_supported_colors:
-        bb_hue_control = MinimalHUEControl()
-        bb_hue_control.list_supported_colors()
+        MinimalHUEControl.list_supported_colors()
         _exit_gracefully()
 
-    _list_known_bubls = _cli_arguments['--list-bulbs']
-    if _list_known_bubls:
-        bb_hue_control = MinimalHUEControl()
-        bb_hue_control.list_known_bulbs()
-        _exit_gracefully()
+    _bridge_address = MinimalHUEControl.DEFAULT_HUE_BRIDGE_ADDRESS_STRING
+    if _cli_arguments['--address'] is not None:
+        _bridge_address = _cli_arguments['--address']
 
-
-    _bulb_name = _cli_arguments['--bulb']
-
-    if _cli_arguments['--debug']:
-        logger.setLevel(logging.DEBUG)
-
-    _bridge_address = _cli_arguments['--address']
-    # if _cli_arguments['--address'] is not None:
-    #     _bridge_address = _cli_arguments['--address']
-
-    bb_hue_control = MinimalHUEControl(_bridge_address=_bridge_address)
+    minimal_hue_control = MinimalHUEControl(_bridge_address=_bridge_address)
 
     _list_known_bulbs = _cli_arguments['--list-bulbs']
     if _list_known_bulbs:
-        bb_hue_control.list_known_bulbs()
+        minimal_hue_control.list_known_bulbs()
         _exit_gracefully()
 
     _connect_application = _cli_arguments['--connect']
     if _connect_application:
-        bb_hue_control.connect_application()
+        minimal_hue_control.connect_application()
         _exit_gracefully()
+
+    _bulb_name = _cli_arguments['--bulb']
 
     _turn_off_bulb = _cli_arguments['--turn-off']
     if _turn_off_bulb:
-        bb_hue_control.turn_off_bulb(_bulb_name)
+        minimal_hue_control.turn_off_bulb(_bulb_name)
         _exit_gracefully()
 
     _set_color = _cli_arguments['--set-color']
     if _set_color:
-        bb_hue_control.set_bulb_color(_bulb_name, _set_color)
+        minimal_hue_control.set_bulb_color(_bulb_name, _set_color)
         _exit_gracefully()
+
 
 def _exit_gracefully():
     logger.info('exit gracefully')
@@ -143,48 +100,43 @@ class MinimalHUEControl(object):
     DEFAULT_LOG_LEVEL = logging.INFO
     DEFAULT_HUE_USERNAME = 'developer'
 
-    Color = collections.namedtuple('Color', 'name, rgb_value')
+    Color = collections.namedtuple('Color', 'name, rgb')
+
+    # define and register the supported colors in RGB representation
+    colors = [
+        Color('WHITE', (255, 255, 255)),
+        Color('RED', (255, 0, 0)),
+        Color('GREEN', (0, 255, 0)),
+        Color('BLUE', (0, 0, 255)),
+        Color('CYAN', (0, 255, 255)),
+        Color('MAGENTA', (255, 0, 255)),
+        Color('YELLOW', (255, 255, 0))
+    ]
+
+    @staticmethod
+    def list_supported_colors():
+        logger.info("INFO list supported colors")
+        for color in MinimalHUEControl.colors:
+            logger.info("  * %s" % repr(color))
 
     def __init__(self,
-                 _bulb_name=None,
                  _bridge_address=DEFAULT_HUE_BRIDGE_ADDRESS_STRING,
                  _user_name=DEFAULT_HUE_USERNAME):
 
-        self._bulb_name = _bulb_name
         self._bridge_address = _bridge_address
-
         self._hue_bridge = phue.Bridge(_bridge_address, _user_name)
-
-
-
-        # define and register the supported colors in RGB representation
-        self._colors = []
-        self._colors.append(self.Color('white', (255, 255, 255)))
-        self._colors.append(self.Color('red', (255, 0, 0)))
-        self._colors.append(self.Color('green', (0, 255, 0)))
-        self._colors.append(self.Color('blue', (0, 0, 255)))
-        self._colors.append(self.Color('cyan', (0, 255, 255)))
-        self._colors.append(self.Color('magenta', (255, 0, 255)))
-        self._colors.append(self.Color('yellow', (255, 255, 0)))
-
-
-    def list_supported_colors(self):
-
-        logger.debug("DEBUG list supported colors")
-        logger.info("INFO list supported colors")
-        logger.error("ERROR list supported colors")
-
-        # for color in self._colors:
-        #     logger.info(" * %s" % repr(color))
-
 
     def set_bulb_color(self, _bulb_name, _color):
 
-        if _color in self._colors[0]:
-            logger.info("set color %s for bulb %s" % (_color, _bulb_name))
-            # self._hue_bridge
+        print "[TODO] add lib call"
+
+        for _valid_color in self.colors:
+            if _valid_color[0] == _color:
+                logger.info("set color %s for bulb %s" % (_color, _bulb_name))
+                break
         else:
-            logger.error("color %s unknown, exit immediately" % _color)
+            logger.error("color %s unknown, do not set anything" % _color)
+
 
 
         # TODO(casasam): this seems to be better, check this lib
@@ -224,24 +176,23 @@ class MinimalHUEControl(object):
         logger.info("connect application to bridge")
         # self._hue_bridge.connect()
         # logger.error('Failed to open file', exc_info=True)
+        print "[TODO] add lib call"
 
     def turn_off_bulb(self, _bulb_name):
         logger.info("turn off bulb %s" % _bulb_name)
-
+        print "[TODO] add lib call"
 
     def list_known_bulbs(self):
         logger.info("list the bulbs known by the HUE bridge")
-
+        print "[TODO] add lib call"
     #     for light in bridge.lights:
     # light.on = True
     # light.hue = 0
     #
 
-
-
     def is_connected(self, bulb_name):
         # try something with getaip here...
-        pass
+        print "[TODO] add lib call"
 
 
 

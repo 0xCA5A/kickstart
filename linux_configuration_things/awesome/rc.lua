36c55ac285c9101c6041ff3f492d464e1d09c0b6
Samuel Casa <samuel.casa@neratec.com>
2015-01-18 16:38:02 +0100

    [awesome] configuration modified
    Signed-off-by: Samuel Casa <samuel.casa@neratec.com>

--- linux_configuration_things/awesome/rc.lua
+++ linux_configuration_things/awesome/rc.lua
@@ -354,7 +354,8 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { size_hints_honor = false,
+                     border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = true,
                      keys = clientkeys,

151f6fa1e3874aff381245b0329263263f65503b
Samuel Casa <samuel.casa@neratec.com>
2015-01-21 12:12:27 +0100

    [awesome] default keybinding to switch screens changed
    Signed-off-by: Samuel Casa <samuel.casa@neratec.com>

--- linux_configuration_things/awesome/rc.lua
+++ linux_configuration_things/awesome/rc.lua
@@ -242,8 +242,8 @@ globalkeys = awful.util.table.join(
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
     awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+    awful.key({ modkey, "Control" }, "Right", function () awful.screen.focus_relative( 1) end),
+    awful.key({ modkey, "Control" }, "Left", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
         function ()
